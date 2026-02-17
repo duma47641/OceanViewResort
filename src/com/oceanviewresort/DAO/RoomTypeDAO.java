7347e50462f474f48b7c4fce660fa473f2906fcd
Umasha Dewmini <duma47641@gmail.com>
2026-02-17 23:09:01 +0530

    This is my first initial commit for this project.

--- src/com/oceanviewresort/DAO/RoomTypeDAO.java
+++ src/com/oceanviewresort/DAO/RoomTypeDAO.java
@@ -1,15 +1,4 @@
 package com.oceanviewresort.DAO;
 
-import com.oceanviewresort.Models.RoomType;
-import java.util.List;
-
-public interface RoomTypeDAO {
-
-    boolean addRoomType(RoomType roomType);
-
-    List<RoomType> getAllRoomTypes();
-
-    RoomType getRoomTypeById(int id);
-
-    boolean deleteRoomType(int id);
+public class RoomTypeDAO {
 }
--- src/com/oceanviewresort/Models/RoomType.java
+++ src/com/oceanviewresort/Models/RoomType.java
@@ -2,33 +2,4 @@ package com.oceanviewresort.Models;
 
 public class RoomType {
 
-    private int id;
-    private String name;
-
-    public RoomType() {}
-
-    public RoomType(String name) {
-        this.name = name;
-    }
-
-    public RoomType(int id, String name) {
-        this.id = id;
-        this.name = name;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
 }
