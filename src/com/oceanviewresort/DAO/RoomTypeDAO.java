086da75016c927531d2beac12a7d1c86a2abcdb0
Umasha Dewmini <duma47641@gmail.com>
2026-02-17 22:13:44 +0530

    I created the RoomType Model class and created a RoomType model in there. And then, I created a RoomTypeDAO interface in DAO package and now committing them to GitHub.

---
+++ src/com/oceanviewresort/DAO/RoomTypeDAO.java
@@ -0,0 +1,15 @@
+package com.oceanviewresort.DAO;
+
+import com.oceanviewresort.Models.RoomType;
+import java.util.List;
+
+public interface RoomTypeDAO {
+
+    boolean addRoomType(RoomType roomType);
+
+    List<RoomType> getAllRoomTypes();
+
+    RoomType getRoomTypeById(int id);
+
+    boolean deleteRoomType(int id);
+}
